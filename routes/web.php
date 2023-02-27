3397abc6df9412284be059994407484f4188a8ce
Jordi Vila <vjordi10@gmail.com>
2023-02-27 22:48:19 +0100

    Update routes to require auth

--- routes/web.php
+++ routes/web.php
@@ -22,18 +22,18 @@ Route::get('/', function () {
 });
 
 Route::get('/home', [TicketController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
-Route::get('/tickets', [TicketController::class, 'index'])->middleware(['auth', 'verified'])->name('tickets');
-Route::get('/tickets/create', [TicketController::class, 'create'])->name('create');
-Route::post('/tickets/create', [TicketController::class, 'store'])->name('store');
-Route::post('/tickets/{ticket}', [TicketController::class, 'update'])->name('update');
-Route::get('/tickets/delete/{ticket}', [TicketController::class, 'delete'])->name('delete');
-Route::post('/tickets/delete/{ticket}', [TicketController::class, 'destroy'])->name('destroy');
-Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/tickets', [TicketController::class, 'index'])->middleware(['auth', 'verified'])->name('tickets');
+    Route::get('/tickets/create', [TicketController::class, 'create'])->name('create');
+    Route::post('/tickets/create', [TicketController::class, 'store'])->name('store');
+    Route::post('/tickets/{ticket}', [TicketController::class, 'update'])->name('update');
+    Route::get('/tickets/delete/{ticket}', [TicketController::class, 'delete'])->name('delete');
+    Route::post('/tickets/delete/{ticket}', [TicketController::class, 'destroy'])->name('destroy');
+    Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('show');
 });
 
 Auth::routes();
